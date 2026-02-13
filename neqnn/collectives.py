c8a87584772964d6369d97c50591f3f362a3d6f1
mcbal <matthiascbal@gmail.com>
2026-02-13 12:54:44 +0100

    More scaffold

---
+++ neqnn/collectives.py
@@ -0,0 +1,4 @@
+# topology of collective?
+# for every module: markov blankets contain sense / act interfaces separating internal from external
+# hmm maybe try loop
+# every module needs to be able to sense possible effect of action on external
